e443c5e24ff88ac07cc1072d26dc93b81ef9e813
Antoine Vancollie <avancoll@fu-r4-p1.s19.be>
2023-07-03 15:52:11 +0200

    library added

---
+++ includes/cub.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avancoll <avancoll@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/03 15:48:22 by avancoll          #+#    #+#             */
+/*   Updated: 2023/07/03 15:51:11 by avancoll         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB_H
+# define CUB_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <math.h>
+# include "mlx.h"
+
+
+#endif
